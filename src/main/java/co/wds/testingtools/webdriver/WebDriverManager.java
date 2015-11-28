5d7b2d2ac91eaea0edfd7d37a963df6080a2bd9a
sega <wds-sfc@globallogic.com>
2015-11-28 17:55:39 +0200

    IL [CA-860]: fixed WebDriverManager.

--- src/main/java/co/wds/testingtools/webdriver/WebDriverManager.java
+++ src/main/java/co/wds/testingtools/webdriver/WebDriverManager.java
@@ -46,21 +46,28 @@ public class WebDriverManager {
 
     private static <T> T getProperty(String name, Class<T> valueClass, String defaultValue) {
         String value = System.getProperty(name);
-        if (value == null) {
-            String envName = name.replaceAll("\\W", "_");
-            value = System.getenv(envName);
-        }
-        if (value == null) {
-            value = defaultValue;
-        }
-        if (value != null) {
+        try {
+            if (value == null) {
+                String envName = name.replaceAll("\\W", "_");
+                value = System.getenv(envName);
+            }
+            if (value == null) {
+                value = defaultValue;
+            }
             if (valueClass == Boolean.class) {
                 return (T) Boolean.valueOf(value);
-            } else if (valueClass == Integer.class) {
-                return (T) Integer.valueOf(value);
+            } else if (value != null) {
+                if (valueClass == Integer.class) {
+                    return (T) Integer.valueOf(value);
+                }
             }
+            return (T) value;
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            System.out.println("Property " + name + "=" + value);
         }
-        return (T) value;
     }
 
     public final static int APP_PORT = getProperty("http.port", Integer.class, "3333");
